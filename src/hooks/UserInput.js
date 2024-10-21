7d19939b309d7c7e36e302ce5b930bd535a4d026
angellubis2404 <134248067+angellubis2404@users.noreply.github.com>
2024-10-21 08:20:51 +0700

    hooks

---
+++ src/hooks/UserInput.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+function useUserInput(initialState = {}) {
+  const [userInput, setUserInput] = useState(initialState);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setUserInput((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const resetUserInput = () => {
+    setUserInput(initialState);
+  };
+
+  return {
+    userInput,
+    handleInputChange,
+    resetUserInput,
+    setUserInput,
+  };
+}
+
+export default useUserInput;
